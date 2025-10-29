aedeccc675ec5735257d9b0534a98db9d170e0b4
dwi fakhri chusaini <fakhri.chusaini01@gmail.com>
2025-10-29 23:43:00 +0700

    fixing next auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,9 @@ import { DefaultUser } from 'next-auth';
 import { DefaultJWT } from 'next-auth/jwt';
 
 interface User extends DefaultUser {
+  id: string;
+  email: string;
+  full_name: string;
   role: UserRole;
   phone: string;
   birth: string;
@@ -14,6 +17,20 @@ interface User extends DefaultUser {
 }
 
 declare module 'next-auth' {
+  interface User {
+    id: string;
+    email: string;
+    full_name: string;
+    role: UserRole;
+    phone: string;
+    birth: string;
+    domicile: string;
+    country: string;
+    country_code: string;
+    gender: string;
+    linkedin: string;
+  }
+
   interface Session {
     user: {
       id: string;
