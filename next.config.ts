ab6a5b1719f45f463cb6ede7a05524d151638b92
dwi fakhri chusaini <fakhri.chusaini01@gmail.com>
2025-11-18 13:03:30 +0700

    fix: prisma config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/**/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
